b868b475892c47222ca8e78549870f803852281f
Jakub Klama <jakub@conclusive.pl>
2022-02-15 18:21:49 +0100

    Add WHLE-LS1088A support.

--- customize/0006-extlinux.sh
+++ customize/0006-extlinux.sh
@@ -3,5 +3,5 @@
 mkdir -p ${ROOTDIR}/boot/extlinux
 sed \
     -e "s|@UBUNTU_VERSION@|${UBUNTU_VERSION}|g" \
-    -e "s|@KERNEL_BOOTARGS|${KERNEL_BOOTARGS}|g" \
+    -e "s|@KERNEL_BOOTARGS@|${KERNEL_BOOTARGS}|g" \
     ${SRCROOT}/etc/extlinux.conf.in > ${ROOTDIR}/boot/extlinux/extlinux.conf
