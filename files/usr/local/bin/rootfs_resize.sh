2f6ed6d26eff81e8dc1a73bed59e52d81b19e22e
Piotr Glinianowicz <piotrgl@conclusive.pl>
2024-01-04 17:49:45 +0100

    In bash script use syntax alternative to backticks

--- files/usr/local/bin/rootfs_resize.sh
+++ files/usr/local/bin/rootfs_resize.sh
@@ -6,9 +6,9 @@ if [ -f $SENTINEL ]; then
     exit
 fi
 
-PARTNAME=`lsblk -o mountpoint,kname,pkname | grep -e "^/\s.*" | tr -s ' ' | cut -d ' ' -f2`
-DEVNAME=`lsblk -o mountpoint,kname,pkname | grep -e "^/\s.*" | tr -s ' ' | cut -d ' ' -f3`
-START=`cat /sys/block/$DEVNAME/$PARTNAME/start`
+PARTNAME=$(lsblk -o mountpoint,kname,pkname | grep -e "^/\s.*" | tr -s ' ' | cut -d ' ' -f2)
+DEVNAME=$(lsblk -o mountpoint,kname,pkname | grep -e "^/\s.*" | tr -s ' ' | cut -d ' ' -f3)
+START=$(cat /sys/block/$DEVNAME/$PARTNAME/start)
 
 growpart /dev/$DEVNAME 1
 
