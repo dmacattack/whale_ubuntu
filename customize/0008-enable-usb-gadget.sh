3ffc617544935dd126897a612867b44653036a5f
piotrgl <piotrgl@conclusive.pl>
2024-01-04 15:20:53 +0100

    Move the usb-gadget.service file to kstr directory as it's a kestrel specific file.

--- customize/0008-enable-usb-gadget.sh
+++ customize/0008-enable-usb-gadget.sh
@@ -4,7 +4,4 @@ mkdir -p ${ROOTDIR}/etc/modules-load.d/
 touch ${ROOTDIR}/etc/modules-load.d/libcomposite.conf
 /bin/sh -c 'echo "libcomposite"' >> ${ROOTDIR}/etc/modules-load.d/libcomposite.conf
 
-mkdir -p ${ROOTDIR}/etc/systemd/system/
-cp ${SRCROOT}/etc/systemd/system/usb-gadget.service ${ROOTDIR}/etc/systemd/system/usb-gadget.service
-
 ${CHROOT_CMD} systemctl enable usb-gadget.service
