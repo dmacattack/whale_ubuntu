5237bfa339649824e62673e41cabaf907fabf12e
piotrgl <piotrgl@conclusive.pl>
2024-01-04 15:23:21 +0100

    Resize rootfs at first boot.
    Reisze rootfs to max possible size. Use "growpart" as it's the only tool that seems to correctly handle this operation when run at boot by systemd.

---
+++ customize/0009-enable-rootfs-resize.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+${CHROOT_CMD} systemctl enable rootfs-resize.service
+
---
+++ files/usr/local/bin/rootfs_resize.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+SENTINEL=/var/lib/rootfs-resized
+if [ -f $SENTINEL ]; then
+    echo "$SENTINEL exists - was the root partition already resized?"
+    exit
+fi
+
+PARTNAME=`lsblk -o mountpoint,kname,pkname | grep -e "^/\s.*" | tr -s ' ' | cut -d ' ' -f2`
+DEVNAME=`lsblk -o mountpoint,kname,pkname | grep -e "^/\s.*" | tr -s ' ' | cut -d ' ' -f3`
+START=`cat /sys/block/$DEVNAME/$PARTNAME/start`
+
+growpart /dev/$DEVNAME 1
+
+partprobe /dev/$DEVNAME
+resize2fs /dev/$PARTNAME
+
+touch $SENTINEL
+
