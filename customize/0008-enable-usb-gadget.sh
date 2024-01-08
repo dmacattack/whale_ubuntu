a116a6fc7b19fb76288544db8f64153378740334
Piotr Glinianowicz <piotrgl@conclusive.pl>
2024-01-08 10:41:13 +0100

    Make the usb-gadget.service optional
    Make the builds for whle pass by making usb-gadget.service existance optional.

--- customize/0008-enable-usb-gadget.sh
+++ customize/0008-enable-usb-gadget.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# This customization is optional as usb-gadget.service does not exist on every board.
+OPTIONAL=1
+
 mkdir -p ${ROOTDIR}/etc/modules-load.d/
 touch ${ROOTDIR}/etc/modules-load.d/libcomposite.conf
 /bin/sh -c 'echo "libcomposite"' >> ${ROOTDIR}/etc/modules-load.d/libcomposite.conf
 
-${CHROOT_CMD} systemctl enable usb-gadget.service
+${CHROOT_CMD} systemctl enable usb-gadget.service || [ $OPTIONAL ]
+
